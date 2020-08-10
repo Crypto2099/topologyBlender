ee0bbd921c706820345b3e2c876bbed57fcab995
Crypto2099 <adam@crypto2099.io>
2020-08-10 20:29:54 +0000

    Duplicate entry consolidation/flattening via temporary file

--- blend.sh
+++ blend.sh
@@ -4,4 +4,6 @@ source config
 echo "Checking for $max_desired_peers peers"
 echo "Finished product will be at $output_topology_path"
 
-jq -s '.[0].Producers=([.[].Producers]|flatten)|.[0]' "${private_topology_path}" "${public_topology_path}"
+jq -s '.[0].Producers=([.[].Producers]|flatten)|.[0]' "${private_topology_path}" "${public_topology_path}" > $tmp_file
+jq '.Producers=(flatten | group_by(.addr) | map(add))' "${tmp_file}" > $output_topology_path
+rm "${tmp_file}"
